cbd9374f8ccc9cc8f296b065dda33b3c0e730f99
alrz14tomcat <alirezaarzcom7@gmail.com>
2025-01-16 02:29:53 +0330

    damn editting has been completed in tags too

--- TodoMain.py
+++ TodoMain.py
@@ -423,13 +423,14 @@ class Tagscreen(Screen):
         self.add_widget(Button(text="go back", size=(dp(100), dp(50)), size_hint=(None, None),
                                 pos_hint={"right": 1, "top": 0.99}, background_color='darkcyan',background_normal="",on_press=self.editback))
 
-        self.labeltx = TextInput(hint_text=f"{tags[tag_id][0]}", halign='center', font_size='20sp',pos_hint={"right": 0.80, "top": 0.98},
-                                    size=(dp(500), dp(40)), size_hint=(None, None), background_color='aquamarine',background_normal="", multiline=False)
+        self.labeltx = TextInput(hint_text=f"{tags[tag_id][0]}",halign='center', font_size='20sp',pos_hint={"right": 0.55, "top": 0.83},
+                                 size=(dp(400), dp(40)), size_hint=(None, None), background_color='aquamarine',
+                                 background_normal="", multiline=False)
         self.add_widget(self.labeltx)
 
-        self.descriptiontx = TextInput(hint_text=f"{tags[tag_id][1]}", halign='center', font_size='20sp',pos_hint={"right": 0.64, "top": 0.85},
-                                           size=(dp(500), dp(430)), size_hint=(None, None),background_color='aquamarine',
-                                           background_normal="", multiline=True)
+        self.descriptiontx = TextInput(hint_text=f"{tags[tag_id][1]}", halign='center', font_size='20sp',pos_hint={"right": 0.55, "top": 0.75},
+                                       size=(dp(400), dp(300)), size_hint=(None, None), background_color='aquamarine',
+                                       background_normal="", multiline=True)
         self.add_widget(self.descriptiontx)
 
         self.savepool = Button(text="save", size=(dp(80), dp(40)), size_hint=(None, None),
