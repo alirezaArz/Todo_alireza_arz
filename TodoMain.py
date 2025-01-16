43d39be38c3eeedf69fa2a92f90a58429a36b465
alrz14tomcat <alirezaarzcom7@gmail.com>
2025-01-16 23:37:41 +0330

    working on finished

--- TodoMain.py
+++ TodoMain.py
@@ -371,7 +371,6 @@ class Finishedlayout(PageLayout):
         self.add_widget(self.todo_scroll)
 
 
-
 class Scrolldone(ScrollView):
     def __init__(self, screen_manager,uppercl, **kwargs):
         super().__init__(**kwargs)
